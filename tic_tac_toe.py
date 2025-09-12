3d77fcb83c1022911f48b13468a379559fc6b86b
robert <murabacha@proton.me>
2025-09-12 21:43:17 +0300

    implemented a smarter computer choice selector

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -10,7 +10,8 @@ if computer_choice == "[red]X[/red]":
     player_choice = "[purple]O[/purple]"
 elif computer_choice == "[purple]O[/purple]":
     player_choice = "[red]X[/red]"
-print(f'you are {player_choice} and the computer is {computer_choice}')
+print('welcome to tic tac toe\n')
+print(f'you are  {player_choice}  and the computer is  {computer_choice} \n')
 position_1_1 = "1"#1
 position_1_2 = "2"#2
 position_1_3 = "3"#3
@@ -24,48 +25,41 @@ empty_positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 positions_empty = False
 end_game = False
 select_corner = True
+close_game = False
 def compare_positions(position_1_1, position_1_2, position_1_3, position_2_1, position_2_2, position_2_3, position_3_1, position_3_2, position_3_3):
     global end_game,empty_positions
     if position_1_1 == position_1_2 == position_1_3:
         winner = position_1_1
         end_game = True
         return winner
-       
     elif position_2_1 == position_2_2 == position_2_3:
         winner = position_2_1
         end_game = True
         return winner
-        
     elif position_3_1 == position_3_2 == position_3_3 :
         winner = position_3_1
         end_game = True
         return winner
-        
     elif position_1_1 == position_2_1 == position_3_1:
         winner = position_1_1
         end_game = True
         return winner
-        
     elif position_1_2 == position_2_2 == position_3_2:
         winner = position_1_2
         end_game = True
         return winner
-        
     elif position_1_3 == position_2_3 == position_3_3:
         winner = position_1_3
         end_game = True
         return winner
-        
     elif position_1_1 == position_2_2 == position_3_3:
         winner = position_1_1
         end_game = True
         return winner
-        
     elif position_1_3 == position_2_2 == position_3_1:
         winner = position_1_3
         end_game = True
         return winner
-        
     else:
         if not empty_positions:
             winner = 'it was a tie'
@@ -110,6 +104,7 @@ def computer_position():
                     position_3_3 = computer_choice
                     select_corner = False
                     return choice
+                return choice
             else:
                 choice = random.choice(empty_positions)
                 empty_positions.remove(choice)
@@ -129,6 +124,7 @@ def computer_position():
                     position_3_2 = computer_choice
                     select_corner = False
                     return choice
+                return choice
         if not select_corner:
             choice = random.choice(empty_positions)
             empty_positions.remove(choice)
@@ -163,7 +159,6 @@ def get_player_position():
             if choice not in empty_positions:
                 print("this position is already taken\n")
             else:
-            
                 empty_positions.remove(choice) 
                 if choice == "1":
                     position_1_1 = player_choice
@@ -199,7 +194,7 @@ def play_game():
     #get_computer_position()
     if end_game:
         return
-    print(f'computer has drawn {computer_choice}\n at position  {computer_position()}')
+    print(f'computer has drawn {computer_choice} at position  {computer_position()}')
     winner = compare_positions(position_1_1, position_1_2, position_1_3, position_2_1, position_2_2, position_2_3, position_3_1, position_3_2, position_3_3)
     if winner == player_choice:
         print("\n[bold red]you won[/bold red]\n")
@@ -216,15 +211,15 @@ def play_game():
     get_player_position()
 def play_misere():
     global positions_empty,player_choice,computer_choice,position_1_1, position_1_2, position_1_3, position_2_1, position_2_2, position_2_3, position_3_1, position_3_2, position_3_3,empty_positions
-    #get_computer_position()
+    print('in thin mode the first person to get 3 consecutive marks loses\n')
     if end_game:
         return
-    print(f'computer has drawn {computer_choice}\n at position  {computer_position()}')
+    print(f'computer has drawn  {computer_choice}  at position  {computer_position()}')
     winner = compare_positions(position_1_1, position_1_2, position_1_3, position_2_1, position_2_2, position_2_3, position_3_1, position_3_2, position_3_3)
     if winner == player_choice:
-        print("\n[bold red]you won[/bold red]\n")
+        print("\n[bold red]you lost you had the first 3 consecutive marks[/bold red]\n")
     elif winner == computer_choice:
-        print("\n[bold red]you lost to the computer[/bold red]\n")
+        print("\n[bold red]you won te computer had the first 3 consecutive marks[/bold red]\n")
     else:
         if end_game:
             print("\n[bold red]its a draw[/bold red]\n")
@@ -235,11 +230,17 @@ def play_misere():
         return
     get_player_position()
 while True:
+    if close_game:
+        break
     while True:
-        print('enter the mode you want to play\n1: for normal tic-tac-toe\n2: for misere tic-tac-toe')
+        print('enter the mode you want to play\n1: for normal tic-tac-toe\n2: for misere tic-tac-toe\n3: to exit')
         try:
             mode = int(input())
-            if mode == 1:
+        except ValueError:
+            print('\n[bold red]i can see you entering letters[/bold red]\n')
+            print('\n[bold red]please enter a valid number[/bold red]\n')
+        if mode == 1:
+            while True:
                 if positions_empty :
                     break
                 if end_game:
@@ -247,7 +248,8 @@ while True:
                     break
                 play_game()
                 continue
-            if mode == 2:
+        elif mode == 2:
+            while True:
                 if positions_empty :
                     break
                 if end_game:
@@ -255,32 +257,34 @@ while True:
                     break
                 play_misere()
                 continue
-            else:
-                print('\n[bold red]please enter a valid number[/bold red]\n')
-        except ValueError:
-            print('\n[bold red]i can see you entering letters[/bold red]\n')
+        elif mode == 3:
+            print('\n[bold red]goodbye[/bold red]\n')
+            close_game = True
+            break
+        else:
             print('\n[bold red]please enter a valid number[/bold red]\n')
-    print('do you want to play again? enter (no ) to exit')
-    again = input().strip().lower()
-    if again == 'no':
-        break
-    else:
-        position_1_1 = "1"#1
-        position_1_2 = "2"#2
-        position_1_3 = "3"#3
-        position_2_1 = "4"#4
-        position_2_2 = "5"#5
-        position_2_3 = "6"#6
-        position_3_1 = "7"#7
-        position_3_2 = "8"#8
-        position_3_3 = "9"#9
-        empty_positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        positions_empty = False
-        end_game = False
-        select_corner = True
-        computer_choice = random.choice(inputs)
-        if computer_choice == "[red]X[/red]":
-            player_choice = "[purple]O[/purple]"
-        elif computer_choice == "[purple]O[/purple]":
-            player_choice = "[red]X[/red]"
-        print(f'you are {player_choice} and the computer is {computer_choice}')
+            continue
+        print('do you want to play again? enter (no ) to exit')
+        again = input().strip().lower()
+        if again == 'no':
+            break
+        else:
+            position_1_1 = "1"#1
+            position_1_2 = "2"#2
+            position_1_3 = "3"#3
+            position_2_1 = "4"#4
+            position_2_2 = "5"#5
+            position_2_3 = "6"#6
+            position_3_1 = "7"#7
+            position_3_2 = "8"#8
+            position_3_3 = "9"#9
+            empty_positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+            positions_empty = False
+            end_game = False
+            select_corner = True
+            computer_choice = random.choice(inputs)
+            if computer_choice == "[red]X[/red]":
+                player_choice = "[purple]O[/purple]"
+            elif computer_choice == "[purple]O[/purple]":
+                player_choice = "[red]X[/red]"
+            print(f'you are {player_choice} and the computer is {computer_choice}')
